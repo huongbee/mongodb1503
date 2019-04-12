22f4850c3f5a89e5e198a2125c31576053944ea6
Huong <huongnguyenak96@gmail.com>
2019-04-12 18:44:04 +0700

    connect db

--- index.js
+++ index.js
@@ -1,38 +1,12 @@
-/**
- * users
- *      id
- *      username
- *      fullname
- *      phone
- * roles
- *      id
- *      user_id
- *      
- * users {
- *      id
- *      username
- *      fullname
- *      phone
- *      roles [
- *          {id:1}
- *          {id:2, name:...}
- *      ]
- * }
- * 
- * sql
- * users 
- *  id:1
- *  username: abc
- *  pass: 12345
- *  phone: null
- *  address: null
- * 
- *  id:2
- *  username: abc
- *  pass: 12345
- *  phone:12234
- *  address:1234
- * 
- * 
- * 
- */
+const mongoose = require('mongoose')
+
+
+mongoose.connect('mongodb://127.0.0.1/mongoose1503',{
+    'useNewUrlParser': true
+});
+mongoose.connection
+.then(()=>{
+    console.log('DB Connected!')
+})
+.catch(err=>console.log(err.message))
+
