cfd453da486bd92bf4ecf6eb840a81c32140db33
Huong <huongnguyenak96@gmail.com>
2019-04-12 18:59:29 +0700

    Schema

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 
-
 mongoose.connect('mongodb://127.0.0.1/mongoose1503',{
     'useNewUrlParser': true
 });
@@ -10,3 +9,21 @@ mongoose.connection
 })
 .catch(err=>console.log(err.message))
 
+const Schema = mongoose.Schema
+const UserSchema = new Schema({
+    username: {
+        type:String,
+        unique: true,
+        require:true
+    },
+    password : {
+        type : String,
+        require:true
+    },
+    birthdate: Date,
+    phone:{
+        type: String,
+        default: '098754321'
+    }
+})
+const User = mongoose.model('user',UserSchema)
