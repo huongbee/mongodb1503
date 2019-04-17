3fb7fb00b7aac5c044e313e188f9daa835e94902
Huong <huongnguyenak96@gmail.com>
2019-04-17 18:26:25 +0700

    query

--- index.js
+++ index.js
@@ -27,10 +27,67 @@ mongoose.connection
 //     if(err) return console.log(err.message)
 // })
 
+// User.insertMany([
+//     {
+//         username: 'teonguyen',
+//         password: '12345678'
+//     },
+//     {
+//         username: 'tinguyen',
+//         password: '12345678'
+//     }
+// ])
+// .then(users=>console.log(users))
+// .catch(err=>console.log(err))
+
+// SELECT * FROM users 
+// User.find((err,res)=>{
+//     if(err) return;
+//     console.log(res)
+// })
+
+// User.find()
+// .then(users=>console.log(users))
+// .catch(err=>console.log(err))
+
+// SELECT * FROM users LIMIT 0,1
+// User.findOne()
+// .then(user=>console.log(user))
+// .catch(err=>console.log(err))
+
+// SELECT * FROM users WHERE id='12343' 
+//5cb082737db7a60795a24a9d
+// User.findById('5cb082737db7a60795a24a9d')
+// .then(user=>console.log(user))
+// .catch(err=>console.log('Error: '+err.message))
+
+//SELECT * FROM users WHERE phone='098754321' AND username='admin'
+// User.findOne({
+//     phone: '098754321',
+//     username: 'admin'
+// })
+// .then(users=>console.log(users))
+// .catch(err=>console.log(err))
+
+// SELECT * FROM users 
+// WHERE (username='guest' OR username='admin') 
+// AND password = '9876543'
+User.find({
+    $or :[ {
+        username:'admin'
+    },{
+        username:'guest'
+    }],
+    password: {
+        $eq : '9876543'
+    }
+})
+.then(users=>console.log(users))
+.catch(err=>console.log(err))
 
 /**
  * Create collection
- * Insert
+ * Insert one/ insert many
  * Select
  * Update
  * Delete
